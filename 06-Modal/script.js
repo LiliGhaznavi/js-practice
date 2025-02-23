5dd60c35127fbb474040a896684ce5543dd1a477
LiliGhaznavi <ghaznavizahra77@gmail.com>
2025-02-23 01:35:08 -0800

    feat(06-Modal): Add modal functionality with open and close actions

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const btnCloseModal = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+
+const openModal = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
